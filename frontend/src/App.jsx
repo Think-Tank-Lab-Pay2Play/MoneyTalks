471ab83dd281ff6a06c26e916b75b3cc56d8b890
Gabi Moldovan <moldovan.gabi75@gmail.com>
2025-01-21 17:30:06 +0200

    first push frontend - added the project structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import Welcome from "./pages/welcome/Welcome.jsx";
+
+{/* open the terminal and run: npm install packages */ }
+{/* to run the project run: npm run dev     in the terminal and go to your localhost address shown in the terminal*/ }
+
+function App() {
+  return (
+    <>
+      <main>
+        <Welcome/>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/welcome/Welcome.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Welcome()
+{
+    const text = ">:D";
+    return (
+        <>
+            <h1>{text}</h1>
+        </>
+    );
+}
