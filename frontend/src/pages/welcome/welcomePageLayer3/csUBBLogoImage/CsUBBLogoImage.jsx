f0939092f937b3e76c2129e69ab441055e3fe799
Gabi Moldovan <moldovan.gabi75@gmail.com>
2025-02-04 01:08:53 +0200

    push logo raiffeisen si csubb pe layer 3 welcome page

---
+++ frontend/src/pages/welcome/welcomePageLayer3/csUBBLogoImage/CsUBBLogoImage.jsx
@@ -0,0 +1,10 @@
+import welcomePageCsUBBLogo from "../../../../assets/logoCSUBB.png";
+import "./CsUBBLogoImage.css";
+
+export default function CsUBBLogoImage() {
+    return (
+        <div className="wlcPageCsUbbLogo">
+            <img src={welcomePageCsUBBLogo} />
+        </div>
+    );
+}
