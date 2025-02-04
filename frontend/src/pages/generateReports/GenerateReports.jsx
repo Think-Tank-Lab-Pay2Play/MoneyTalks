e1e44211b130a3db11d234bac9700cac116714b6
Gabi Moldovan <moldovan.gabi75@gmail.com>
2025-02-04 19:50:25 +0200

    modificare stil generate reports page

--- frontend/src/pages/generateReports/GenerateReports.jsx
+++ frontend/src/pages/generateReports/GenerateReports.jsx
@@ -3,10 +3,6 @@ import Reports from "./reports/Reports";
 
 export default function GenerateReports()
 {
-    const mockData = {
-        budget: "Buget vs Cheltuieli:\n- Total: 5000 RON\n- Cheltuit: 4500 RON",
-      };
-      
     return (
         <>
             <GeneralTopBar/>
