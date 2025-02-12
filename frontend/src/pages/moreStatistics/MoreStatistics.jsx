a89aa3f88f3d8f12d33dceb55d808801a464cdb2
Gabi Moldovan <moldovan.gabi75@gmail.com>
2025-02-12 03:43:38 +0200

    start implementare acc settings, tutorial, statistics pages

--- frontend/src/pages/moreStatistics/MoreStatistics.jsx
+++ frontend/src/pages/moreStatistics/MoreStatistics.jsx
@@ -1,10 +1,11 @@
+import "./MoreStatistics.css"
+import GeneralTopBar from "../generalTopBar/GeneralTopBar";
 
 
 export default function MoreStatistics() {
-    const fatalError = ">:D statistiq";
     return (
         <>
-            <h1>{fatalError}</h1>
+            <GeneralTopBar/>
             {/*
 Analiza Tendințelor de Cheltuieli
 Descriere: Afișează evoluția cheltuielilor pe categorii (ex: "Cheltuieli cu mâncarea au crescut cu 20% față de luna trecută").
@@ -19,10 +20,6 @@ Harta Geografică a Cheltuielilor
 Descriere: O hartă interactivă care arată unde s-au făcut cele mai multe cheltuieli (ex: magazine, restaurante).
 
 Beneficiu: Utilizatorii pot identifica locurile/zonele unde cheltuie prea mult și să găsească alternative mai ieftine.
-
-
-
-
             */}
         </>
     );
