1dfcd93afce8cde95e48a8d5df3b067f0fe8679a
Gabi Moldovan <moldovan.gabi75@gmail.com>
2025-03-03 19:19:07 +0200

    implementare pagina tutorial

--- frontend/src/pages/tutorial/Tutorial.jsx
+++ frontend/src/pages/tutorial/Tutorial.jsx
@@ -1,12 +1,18 @@
 import PagesBackground from "../components/pages-background/PagesBackground";
 import GeneralTopBar from "../generalTopBar/GeneralTopBar"
 import "./Tutorial.css"
+import VideoPrezentare from "../../assets/prezentare.mp4";
+import ImgBackground from "../../assets/img-background.png";
 
 export default function Tutorial() {
     return (
         <>
-            <GeneralTopBar/>
-            <PagesBackground/>
+            <GeneralTopBar />
+            <PagesBackground />
+            <img src={ImgBackground} className="img-background-tutorial-video" />
+            <video width="1000" controls className="video-prezentare">
+                <source src={VideoPrezentare} type="video/mp4" />
+            </video>
         </>
     );
 }
